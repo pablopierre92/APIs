d0037c718e02f0d45176aa1dc76e201f04a2d908
pablopierre92 <159732257+pablopierre92@users.noreply.github.com>
2025-04-15 15:30:18 -0300

    Action Results e HTTP Status Codes

---
+++ PrimeiraAPI/PrimeiraAPI/Controllers/TesteController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PrimeiraAPI.Controllers
+{
+    [ApiController]
+    [Route("api/demo")]
+    public class TesteController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new Produto { Id = 1, Nome = "Teste" });
+        }
+
+        [HttpGet("{id:int}")]
+        public IActionResult Get(int id)
+        {
+            return Ok(new Produto { Id = 1, Nome = "Teste" });
+        }
+
+        [HttpPost]
+
+        public IActionResult Post(Produto produto)
+        {
+            return CreatedAtAction("Get", new { id = produto.Id }, produto);
+        }
+
+        [HttpPut("{id:int}")]
+        public IActionResult Put(int id, Produto produto)
+        {
+            if (id != produto.Id) return BadRequest();
+
+            return NoContent();
+
+
+        }
+
+        [HttpDelete("{id:int}")]
+        public IActionResult Delete(int id)
+        {
+            
+
+            return NoContent();
+
+
+        }
+    }
+}
