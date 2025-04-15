410007eb6fd8a5e80718685353a9f9a122ecd7da
pablopierre92 <159732257+pablopierre92@users.noreply.github.com>
2025-04-15 15:59:07 -0300

    Formatadores de Resposta

--- PrimeiraAPI/PrimeiraAPI/Controllers/TesteController.cs
+++ PrimeiraAPI/PrimeiraAPI/Controllers/TesteController.cs
@@ -7,25 +7,31 @@ namespace PrimeiraAPI.Controllers
     public class TesteController : ControllerBase
     {
         [HttpGet]
+        [ProducesResponseType(typeof(Produto), StatusCodes.Status200OK)]
         public IActionResult Get()
         {
             return Ok(new Produto { Id = 1, Nome = "Teste" });
         }
 
         [HttpGet("{id:int}")]
+        [ProducesResponseType(typeof(Produto), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public IActionResult Get(int id)
         {
-            return Ok(new Produto { Id = 1, Nome = "Teste" });
+            return Ok(new Produto { Id = id, Nome = "Teste" });
         }
 
         [HttpPost]
-
+        [ProducesResponseType(typeof(Produto), StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public IActionResult Post(Produto produto)
         {
             return CreatedAtAction("Get", new { id = produto.Id }, produto);
         }
 
         [HttpPut("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public IActionResult Put(int id, Produto produto)
         {
             if (id != produto.Id) return BadRequest();
@@ -36,6 +42,8 @@ namespace PrimeiraAPI.Controllers
         }
 
         [HttpDelete("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public IActionResult Delete(int id)
         {
             
