1851f5984f7766f2752173cc0a7aa60bdaa333fd
pablopierre92 <159732257+pablopierre92@users.noreply.github.com>
2025-04-16 22:08:17 -0300

    Validação da entidade

--- ApiFuncional/ApiFuncional/Controllers/ProdutosController.cs
+++ ApiFuncional/ApiFuncional/Controllers/ProdutosController.cs
@@ -18,10 +18,66 @@ namespace ApiFuncional.Controllers
         }
 
         [HttpGet]
-        public async Task<IActionResult<IEnumerable<Produto>>> GetProdutos()
+        public async Task<ActionResult<IEnumerable<Produto>>> GetProdutos()
         {
             return await _context.Produtos.ToListAsync();
         }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<Produto>> GetProduto(int id)
+        {
+            var produto = await _context.Produtos.FindAsync(id);
+
+            return produto;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Produto>> PostProduto(Produto produto)
+        {
+
+            if (!ModelState.IsValid)
+            {
+                //return BadRequest(ModelState);
+                //return ValidationProblem(ModelState);
+
+                return ValidationProblem(new ValidationProblemDetails(ModelState)
+                {
+                    Title = "Um ou mais erros de validação ocorreram!"
+                });
+            }
+
+            _context.Produtos.Add(produto);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetProduto), new { id = produto.Id }, produto);
+        }
+
+        [HttpPut("{id:int}")]
+        public async Task<IActionResult> PutProduto(int id, Produto produto)
+        {
+
+            if (id != produto.Id) return BadRequest();
+
+            if (!ModelState.IsValid) return ValidationProblem(ModelState);
+
+            _context.Update(produto);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id:int}")]
+
+        public async Task<IActionResult> DeleteProduto(int id)
+        {
+            var produto = await _context.Produtos.FindAsync(id);
+
+            _context.Produtos.Remove(produto);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
 
     }
 }
